aabaf8e09a8738eb26315399776a1a21c76b4f6b
hinha <martinuz.dawan9@gmail.com>
2021-09-07 11:22:43 +0700

    changes input form via API. creating handler input output

--- frontend/src/pages/SettingsUser.jsx
+++ frontend/src/pages/SettingsUser.jsx
@@ -1,22 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import Moment from "react-moment";
 
-import * as SettingService from "../services/setting";
+import { UPDATE_USER_SETTING } from "../services/setting";
+import { FormTrello, FormUser, FormRole } from "../modules/form/SettingsForm";
 
-function Settings({ token, credentials }) {
-  const [userSetting, setUserSetting] = useState(null);
+function Settings({ onClickSidebarApi }) {
+  const [getTrelloUser, setTrelloUser] = useState([]);
+  const [optTrelloUserSelected, setOptUserTrello] = useState([]);
+  const [getAccessControl, setAccessControl] = useState({});
+  const [getAccountList, setAccountList] = useState([]);
 
   useEffect(() => {
+    let mounted = true;
     const fetchSetting = async () => {
-      const result = await SettingService.getSettingUser(token, credentials);
-      setUserSetting(result);
-      console.log(result);
+      const result = await onClickSidebarApi(UPDATE_USER_SETTING);
+
+      let optUser = [];
+      if (result.trello) {
+        optUser.push({ value: "", label: "Select ID" });
+        for (let i = 0; i < result.trello.user.length; i++) {
+          const element = result.trello.user[i];
+          optUser.push({ value: element.id, label: element.name });
+        }
+      }
+      setOptUserTrello(optUser);
+      setAccessControl(result.access);
+      setAccountList(result.account);
+      setTrelloUser(result.trello.trello_user);
     };
-    fetchSetting();
-  }, []);
-  console.log("userSetting", userSetting);
+    if (mounted) {
+      fetchSetting();
+    }
+
+    return () => (mounted = false);
+  }, [onClickSidebarApi]);
+
+  const tableListAccount = (newAccount) => {
+    if (newAccount.data) {
+      setAccountList([...getAccountList, newAccount.data]);
+    }
+  };
+
+  const tableListUserTrello = (newUser) => {
+    if (newUser.data) {
+      setTrelloUser([...getTrelloUser, newUser.data]);
+    }
+  };
+
+  // const trelloAddButton = (e) => {
+  //   e.preventDefault();
+  //   onSendMessage({
+  //     eventItem: "setting:trello",
+  //     eventName: "add",
+  //   });
+  // };
 
   return (
     <>
@@ -39,6 +79,7 @@ function Settings({ token, credentials }) {
       </div>
       <section className="content">
         <div className="container-fluid">
+          <ToastContainer />
           <div className="row">
             <div className="col-md-12">
               <div className="row">
@@ -58,11 +99,6 @@ function Settings({ token, credentials }) {
                         Trello
                       </a>
                     </li>
-                    <li className="nav-item">
-                      <a href="#log" className="nav-link" data-toggle="tab">
-                        Log
-                      </a>
-                    </li>
                   </ul>
                 </div>
               </div>
@@ -75,57 +111,10 @@ function Settings({ token, credentials }) {
                           <h3 className="card-title">Register User</h3>
                         </div>
 
-                        <form id="register-account">
-                          <input
-                            type="hidden"
-                            name="csrf"
-                            defaultValue="{{ .Token }}"
-                          />
-                          <div className="card-body">
-                            <div className="form-group">
-                              <label>Full Name</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                name="name"
-                                placeholder="Enter Full Name"
-                              />
-                            </div>
-                            <div className="form-group">
-                              <label>Username</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                placeholder="Enter Username"
-                              />
-                            </div>
-                            <div className="form-group">
-                              <label>Email address</label>
-                              <input
-                                type="email"
-                                className="form-control"
-                                name="email"
-                                placeholder="Enter email"
-                              />
-                            </div>
-                            <div className="form-group">
-                              <label>Password</label>
-                              <input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                placeholder="Password"
-                              />
-                            </div>
-                          </div>
-                          {/* /.card-body */}
-                          <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">
-                              Submit
-                            </button>
-                          </div>
-                        </form>
+                        <FormUser
+                          handleRequestAPI={onClickSidebarApi}
+                          dispatch={tableListAccount}
+                        />
                       </div>
                     </div>
                     <div className="col-md-6">
@@ -133,42 +122,11 @@ function Settings({ token, credentials }) {
                         <div className="card-header">
                           <h3 className="card-title">Assign Role</h3>
                         </div>
-
-                        {/* form start */}
-                        <form id="assign-role">
-                          <div className="card-body">
-                            <div className="form-group">
-                              <label>Select User</label>
-                              <select
-                                className="form-control select2"
-                                style={{ width: "100%" }}
-                                id="opt-select-user"
-                              ></select>
-                            </div>
-                            <div className="form-group">
-                              <label>Select Role</label>
-                              <select
-                                className="form-control select2"
-                                style={{ width: "100%" }}
-                                id="opt-select-role"
-                              ></select>
-                            </div>
-                            <div className="form-group">
-                              <label>Select Permission</label>
-                              <select
-                                className="form-control select2"
-                                style={{ width: "100%" }}
-                                id="opt-select-permission"
-                              ></select>
-                            </div>
-                          </div>
-                          {/* /.card-body */}
-                          <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">
-                              Create
-                            </button>
-                          </div>
-                        </form>
+                        <FormRole
+                          listOptions={getAccessControl}
+                          listAccount={getAccountList}
+                          handleRequestAPI={onClickSidebarApi}
+                        />
                       </div>
                     </div>
                   </div>
@@ -234,27 +192,65 @@ function Settings({ token, credentials }) {
                                 <th>Action</th>
                               </tr>
                             </thead>
-                            <tbody id="user">
-                              <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>
-                                  <div className="text-center">
-                                    <div
-                                      className="spinner-border"
-                                      role="status"
-                                    >
-                                      <span className="sr-only">
-                                        Loading...
+                            <tbody>
+                              {getAccountList &&
+                                getAccountList.map((item, index) => {
+                                  let status;
+                                  if (item.suspend_status === true) {
+                                    status = (
+                                      <span className="badge bg-danger">
+                                        Deactivate
                                       </span>
-                                    </div>
-                                  </div>
-                                </td>
-                                <td></td>
-                                <td></td>
-                              </tr>
+                                    );
+                                  } else {
+                                    status = (
+                                      <span className="badge bg-primary">
+                                        Activate
+                                      </span>
+                                    );
+                                  }
+                                  return (
+                                    <tr key={index}>
+                                      <td>{item.id}</td>
+                                      <td>{item.name}</td>
+                                      <td>{item.username}</td>
+                                      <td>{item.email}</td>
+                                      <td>{status}</td>
+                                      <td>
+                                        <Moment toNow>{item.last_login}</Moment>
+                                      </td>
+                                      {/* <td>{item.created_at}</td> */}
+                                      <td>
+                                        <Moment
+                                          locale="id"
+                                          parse="YYYY-MM-DD HH:mm"
+                                          withTitle
+                                        >
+                                          {item.created_at}
+                                        </Moment>
+                                      </td>
+
+                                      <td>
+                                        <button
+                                          type="submit"
+                                          className="btn btn-primary btn-sm"
+                                          value="${value.id},${value.username}"
+                                        >
+                                          EDIT
+                                        </button>
+                                        <button
+                                          type="submit"
+                                          className="btn btn-danger btn-sm"
+                                          value="${value.id},${value.username}"
+                                          data-toggle="modal"
+                                          data-target="#exampleModal"
+                                        >
+                                          DELETE
+                                        </button>
+                                      </td>
+                                    </tr>
+                                  );
+                                })}
                             </tbody>
                           </table>
                         </div>
@@ -309,8 +305,8 @@ function Settings({ token, credentials }) {
                               </tr>
                             </thead>
                             <tbody>
-                              {userSetting ? (
-                                userSetting.trello_user.map((item, index) => {
+                              {getTrelloUser ? (
+                                getTrelloUser.map((item, index) => {
                                   return (
                                     <tr key={index}>
                                       <td>{(index += 1)}</td>
@@ -340,64 +336,15 @@ function Settings({ token, credentials }) {
                         </div>
 
                         {/* form start */}
-                        <form id="assign-role">
-                          <div className="card-body">
-                            <div className="form-group">
-                              <label>Select User</label>
-                              <select
-                                className="form-control select2"
-                                style={{ width: "100%" }}
-                                id="opt-select-user"
-                              >
-                                {userSetting ? (
-                                  userSetting.user.map((item, index) => {
-                                    return (
-                                      <option
-                                        selected="selected"
-                                        key={index}
-                                        value={item.username}
-                                      >
-                                        {item.id} - {item.name}
-                                      </option>
-                                    );
-                                  })
-                                ) : (
-                                  <option />
-                                )}
-                              </select>
-                            </div>
-                            <div className="form-group">
-                              <label>Board Name</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                name="board_name"
-                                placeholder="Enter Name"
-                              />
-                            </div>
-                            <div className="form-group">
-                              <label>Member ID</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                name="member_id"
-                                placeholder="Enter Id"
-                              />
-                            </div>
-                          </div>
-                          {/* /.card-body */}
-                          <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">
-                              Create
-                            </button>
-                          </div>
-                        </form>
+                        <FormTrello
+                          dispatch={tableListUserTrello}
+                          listOptions={optTrelloUserSelected}
+                          handleRequestAPI={onClickSidebarApi}
+                        />
+                        {/* form end */}
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="tab-pane fade" id="log">
-                  <p>Messages tab content ...</p>
                 </div>
               </div>
             </div>
@@ -409,7 +356,6 @@ function Settings({ token, credentials }) {
 }
 
 const mapStateToProps = (state) => ({
-  socket: state.socket,
   credentials: state.auth.credentials,
   token: state.auth.token,
 });
